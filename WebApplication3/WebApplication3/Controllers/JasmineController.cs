65185dd769904c7ddf2855f4008d1afb8a1a69db
Pushparaj <raju.vellayil@gmail.com>
2020-03-24 14:42:30 +0530

    test

--- WebApplication3/WebApplication3/Controllers/JasmineController.cs
+++ WebApplication3/WebApplication3/Controllers/JasmineController.cs
@@ -7,7 +7,7 @@ namespace WebApplication3.Controllers
     {
         public ViewResult Run()
         {
-            return sView("SpecRunner");
+            return s View("SpecRunner");
         }
     }
 }
