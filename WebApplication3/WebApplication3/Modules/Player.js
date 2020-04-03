8c13a4c1858cdc35ec2a5c779500ccdb9e6ea2d4
Pushparaj <raju.vellayil@gmail.com>
2020-04-03 15:09:12 +0530

    path chages

---
+++ WebApplication3/WebApplication3/Modules/Player.js
@@ -0,0 +1,30 @@
+function Player() {
+}
+Player.prototype.play = function(song) {
+  this.currentlyPlayingSong = song;
+  this.isPlaying = true;
+};
+
+Player.prototype.pause = function() {
+  this.isPlaying = false;
+};
+
+Player.prototype.resume = function() {
+  if (this.isPlaying) {
+    throw new Error("song is already playing");
+  }
+
+  this.isPlaying = true;
+};
+
+Player.prototype.makeFavorite = function() {
+  this.currentlyPlayingSong.persistFavoriteStatus(true);
+};
+function getProduct(id) {
+    $.ajax({
+        type: "GET",
+        url: "/products/" + id,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json"
+    });
+}
