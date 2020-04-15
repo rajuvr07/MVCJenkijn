cb0c3a269ba33065300e232bc159f44097525580
Pushparaj <raju.vellayil@gmail.com>
2020-04-15 17:22:29 +0530

    for failed test case

--- WebApplication3/ClientTest/Test/Ers.UI.Web/Areas/Financials/Views/BankStatementAnalysis/Scripts/mathUtilSpec.js
+++ WebApplication3/ClientTest/Test/Ers.UI.Web/Areas/Financials/Views/BankStatementAnalysis/Scripts/mathUtilSpec.js
@@ -20,7 +20,7 @@ describe("MathUtils", function () {
 
         //Spec for sum operation
         it("should be able to calculate sum of 3 and 5", function () {
-            expect(calc.sum(3, 5)).toEqual(8);
+            expect(calc.sum(3, 5)).toEqual(4);
         });
 
         //Spec for multiply operation
